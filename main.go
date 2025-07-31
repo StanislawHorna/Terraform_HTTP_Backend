7c90aa2a7f8334d1fc6406e05359afd2ac6ac42e
Stanisław Horna <stanislawhorna@outlook.com>
2025-07-31 22:31:05 +0200

    add basic prometheus intrumentation

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"terraform_http_backend/src"
 	"terraform_http_backend/src/handler"
@@ -16,6 +17,9 @@ func main() {
 	r := chi.NewRouter()
 	r.Get("/state/{projectName}/{environment}", handler.GetState)
 	r.Post("/state/{projectName}/{environment}", handler.SetState)
+
+	// Add the /metrics endpoint for Prometheus
+	r.Handle("/metrics", promhttp.Handler())
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		panic(err)
